dc61695f7db5fafb5858c5ccbd39296d729eebb5
Ashokmakwana <am9020682@gmail.com>
2024-02-29 03:09:20 -0800

    Java-chpt-2

---
+++ function.js
@@ -0,0 +1,21 @@
+window.alert(
+    "Wellcome"
+);
+alert(
+    "To This Site!"
+);
+document.write(
+    "This is A java page"
+);
+console.log(
+    "All alert Functions listed _4"
+);
+var alerts="Window.alert,alert,document.write,console.log"
+console.log(
+    alert
+)
+
+var msg="Variables"
+alert(
+    msg
+)
